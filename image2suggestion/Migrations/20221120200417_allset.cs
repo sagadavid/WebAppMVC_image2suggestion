76c7fc7721d97709bf9a75448c8d0b05318ff2ee
sagadavid <epostsaga@gmail.com>
2022-11-20 21:10:23 +0100

    photos per suggestions memorystream to db canceled bytes to mb conversion canceled

---
+++ image2suggestion/Migrations/20221120200417_allset.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace image2suggestion.Migrations
+{
+    public partial class allset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- image2suggestion/Models/Suggestion.cs
+++ image2suggestion/Models/Suggestion.cs
@@ -8,7 +8,6 @@ namespace image2suggestion.Models
         [Key]
         public int Id { get; set; }
 
-        //[RegularExpression(@"^[A-Z0-9]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Title should begin with uppercase and should not contain special character")]
         [StringLength(70, MinimumLength = 3, ErrorMessage = "Title should contain minimum 3 characters.")]
         [Required]
         public string Title { get; set; }
